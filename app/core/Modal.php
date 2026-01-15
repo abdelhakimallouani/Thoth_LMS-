99b7f5991080b9500d038f6480090a36bf7448b1
abdelhakim allouani <abdelhakimallouani@gmail.com>
2026-01-15 15:22:32 +0100

    feat (Modal): implement abstract Modal class with CRUD methods

---
+++ app/core/Modal.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Core;
+
+abstract class Modal
+{
+    protected $db;
+    protected $table;
+    protected $primaryKey = 'id';
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+    public function all()
+    {
+        $stmt = $this->db->prepare("SELECT * FROM {$this->table}");
+        return $stmt->fetchAll();
+    }
+
+    public function find($id)
+    {
+        $sql = $this->db->prepare("SELECT * FROM {$this->table} WHERE {$this->primaryKey} = ?");
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+
+
+    public function create(array $data)
+    {
+
+        $columns = implode(',', array_keys($data));
+        $values = ':' . implode(',:', array_keys($data));
+        $sql = "INSERT INTO {$this->table} ($columns) VALUES ($values)";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $fields = [];
+        foreach ($data as $key => $value) {
+            $fields[] = "$key = :$key";
+        }
+        $sql = "UPDATE {$this->table} SET " . implode(',', $fields) . " WHERE {$this->primaryKey} = :id";
+        $stmt = $this->db->prepare($sql);
+        $data['id'] = $id;
+        return $stmt->execute($data);
+    }
+
+    public function delete($id){
+        $sql = "DELETE FROM {$this->table} WHERE {$this->primaryKey} = ?";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$id]);
+    }
+}
